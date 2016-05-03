beaceaf7781a3fe88edd532ac07f5d5857a32c6f
vincenttran <vincent.tran@basekit.com>
2016-05-03 18:34:12 +0100

    update

--- js/custom.js
+++ js/custom.js
@@ -55,3 +55,13 @@ $(document).ready(function() {
         }
     });
 });
+
+function goFullscreen(id) {
+  var element = document.getElementById(id);       
+  if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+  element.play();
+}
